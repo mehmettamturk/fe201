919ff0ed62d666f7ddc11a6ba6d55e9626647bb6
Mehmet <Memo@.(none)>
2011-07-06 14:05:48 +0300

    [FollowManager.js] FollowManager class implemented..

--- twitter/js/models/FollowManager.js
+++ twitter/js/models/FollowManager.js
@@ -1,13 +1,38 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
-//
-// @author Armagan Amcalar <armagan@tart.com.tr>
-
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Memo
+ * Date: 05.07.2011
+ * Time: 11:38
+ * To change this template use File | Settings | File Templates.
+ */
 var FollowManager = function() {
 
+}
+
+FollowManager.prototype.createFollow = function (follower,following) {
+        var follow = new Follow(follower.id,following.id);
+        localStorage.setObject(follow.id, follow);
 };
 
-FollowManager.prototype.createFollow = function(follower, following) {
-    var follow = new Follow(follower.id, following.id);
-    localStorage.setObject(follow.id, follow);
-}
+FollowManager.prototype.getFollowingByUserId = function (userId) {
+    var array = [];
+    for(var following in localStorage) {
+        following = localStorage.getObject(following);
+        if(following && following.userId && following.userId==userId) {
+            array.push(following)
+        }
+    }
+    return array;
+};
+
+
+FollowManager.prototype.getFollowersByUserId = function (userId) {
+    var array = [];
+    for(var follower in localStorage) {
+        follower = localStorage.getObject(follower);
+        if(follower && follower.userId && follower.userId==userId) {
+            array.push(follower)
+        }
+    }
+    return array;
+};

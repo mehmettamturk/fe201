ed1964eb4e7380df235df7e7ecbf5a1b8a21090a
Mehmet <Memo@.(none)>
2011-07-06 14:06:50 +0300

    [TweetModel] TweetModel class implemented..

--- twitter/js/models/TweetModel.js
+++ twitter/js/models/TweetModel.js
@@ -1,13 +1,38 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
-//
-// @author Armagan Amcalar <armagan@tart.com.tr>
-
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Memo
+ * Date: 05.07.2011
+ * Time: 11:05
+ * To change this template use File | Settings | File Templates.
+ */
 var TweetModel = function() {
 
-};
-
-TweetModel.prototype.postTweet = function(user, body) {
-    var tweet = new Tweet(user, body);
-    localStorage.setObject(tweet.id, tweet);
 }
+
+TweetModel.prototype.postTweet = function (user,body) {
+        var tweet = new Tweet(user.id,body);
+        localStorage.setObject(tweet.id,tweet);
+
+    };
+
+TweetModel.prototype.removeTweet = function (tweetId) {
+        for(var tweetkey in localStorage) {
+        tweet = localStorage.getObject(tweetkey);
+        if(tweet && tweet.id &&tweet.id == tweetId) {
+            localStorage.removeItem(tweetkey);
+            return true;
+        }
+    }
+    return false;
+    };
+
+TweetModel.prototype.getTweetsByUserId = function (userId) {
+   var array = [];
+    for(var tweet in localStorage) {
+     tweet = localStorage.getObject(tweet);
+        if(tweet && tweet.userId && tweet.userId==userId){
+        array.push(tweet);
+        }
+    }
+    return array;
+};

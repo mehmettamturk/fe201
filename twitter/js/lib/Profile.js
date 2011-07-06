5e71928c2fb94576c17a594d8aa781ed267662e9
Mehmet <Memo@.(none)>
2011-07-06 13:05:41 +0300

    [Profile.js] Profile class created..

--- twitter/js/lib/Profile.js
+++ twitter/js/lib/Profile.js
@@ -1,4 +1,14 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
-//
-// @author Armagan Amcalar <armagan@tart.com.tr>
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Memo
+ * Date: 05.07.2011
+ * Time: 11:36
+ * To change this template use File | Settings | File Templates.
+ */
+var Profile = function(userId, bio, photoUrl, location){
+    this.id = tart.getUId();
+    this.userId = userId;
+    this.bio = bio;
+    this.photoUrl = photoUrl;
+    this.location = location;
+};
